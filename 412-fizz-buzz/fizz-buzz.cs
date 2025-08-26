eda80c4f8590d48f718347db623d17c7cb281338
Muhammed Rizal N P <risalsalu15@gmail.com>
2025-08-26 09:57:24 +0530

    Time: 8 ms (24.37%) | Memory: 68.4 MB (27.63%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public IList<string> FizzBuzz(int n) {
+        var list = new List<string>(n);
+        for (int i=1; i<=n; i++){
+            string item =(i % 3==0 , i% 5==0)switch{
+                (true,true)=> "FizzBuzz",
+                (true,false)=> "Fizz",
+                (false,true)=> "Buzz",
+                (false,false)=>i.ToString()
+
+            };
+            list.Add(item);
+        }
+        return list;
+    }
+}
